7f78094c77cfdeb14001d34201ab5eb7ce0bcd58
zocomputer <help@zocomputer.com>
2026-05-08 16:19:29 +0000

    Add CI workflow and patch Package.swift for macOS v15

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "Vitalink",
     platforms: [
-        .macOS(.v26),
+        .macOS(.v15),
     ],
     products: [
         .executable(name: "vitalink", targets: ["VitalinkExec"]),
